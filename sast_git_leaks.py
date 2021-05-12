c1c17d81144be1d73fcbdb18c8145a5d0595fcc0
Fabien Martinez <fabien.martinez@adevinta.com>
2021-05-12 18:23:45 +0200

    [sast-git-leaks]: Move process functions in module
    Signed-off-by: Fabien Martinez <fabien.martinez@adevinta.com>

--- sast_git_leaks.py
+++ sast_git_leaks.py
@@ -9,111 +9,15 @@ Written by Fabien Martinez <fabien.martinez+github@adevinta.com>
 '''
 import sys
 from pathlib import Path
-import subprocess
 import argparse
-from importlib import import_module
 
 from config import variables
 from sast_git_leaks import logger as logging
-from sast_git_leaks import utils
-
-
-def load_tool(tool: dict, path: Path, logger: logging, report_path: Path, loggername: str, limit: int):
-    '''
-    Load tool module then instantiate tool
-    '''
-    for variable in variables.MANDATORY_TOOL_VARIABLES:
-        if variable not in tool:
-            logger.error(f'Unable to find mandatory variable {variable}!')
-            return False
-    try:
-        obj = getattr(
-            import_module(f'sast_git_leaks.tools.{tool["name"]}'),
-            tool['class']
-            )(
-                tool,
-                path,
-                variables.DATA_PATH,
-                report_path,
-                loggername,
-                limit
-            )
-    except Exception as e:
-        logger.error(f'Unable to load {tool["name"]}: {e}', exc_info=True)
-        return False
-    else:
-        logger.info(f'Tool {tool["name"]} loaded')
-        return obj
-
-
-def load_tools(tools_loaded: str, path: Path, logger: logging, report_path: Path, loggername: str, limit: int):
-    '''
-    Check which tools to load from argument
-    '''
-    to_load = tools_loaded.replace(' ', '').lower().split(',')
-    tools = []
-    tools_names = variables.TOOLS.keys()
-    tmp_names = set(tools_names)
-    tools_to_load = dict()
-    if 'all' in to_load:
-        tools_to_load = variables.TOOLS
-    else:
-        for index, value in enumerate(to_load):
-            if value in tmp_names:
-                name = to_load[index]
-                tools_to_load[name] = variables.TOOLS[name]
-    logger.info(f'Loading tools...')
-    for data in tools_to_load.values():
-        obj = load_tool(data, path, logger, report_path, loggername, limit)
-        if obj is False:
-            logger.info(f'Load tools  aborted.')
-            return False
-        tools.append({'name': data['name'], 'object': obj})
-    return tools
-
-
-def process(repo_path, output, volume=None, limit=-1, tools='all'):
-    logger = logging.getLogger(__name__)
-    logger.info(f'Repository to check: {repo_path}')
-    if not repo_path.is_dir():
-        logger.error(f"Wront repo path [{repo_path}]!")
-        return False
-    report_path = Path(f'{output}.csv')
-    logger.info(f'Report path: {report_path}')
-    if volume is not None:
-        variables.DATA_PATH = Path(volume)
-    if not variables.DATA_PATH.exists():
-        logger.info(f'Creating data folder [{variables.DATA_PATH.resolve()}]')
-        try:
-            variables.DATA_PATH.mkdir(parents=True)
-        except Exception as e:
-            logger.error(f'Unable to create data directory [{variables.DATA_PATH.resolve()}]: {e}')
-            return False
-    else:
-        if not variables.DATA_PATH.is_dir():
-            logger.error(f'Unable to find a valid data path for [{variables.DATA_PATH.resolve()}]')
-            return False
-    tools = load_tools(tools, repo_path, logger, report_path, variables.LOG_ENV, limit)
-    if tools is False:
-        return False
-    logger.info(f'Tools loaded: {", ".join([tool["name"] for tool in tools])}')
-    for tool in tools:
-        logger.info(f'Running {tool["name"]}')
-        if not tool['object'].process():
-            logger.error(f'Failed to run {tool["name"]}')
-    data = utils.read_csv(report_path)
-    if not utils.convert_csv_to_json(report_path, Path(output)):
-        logger.error(f'Unable to convert csv file ({report_path.resolve()}) to json file ({args.output})')
-    try:
-        report_path.unlink()
-    except Exception as e:
-        logger.warning(f'Unable to remove {report_path.resolve()}: {e}')
-    return data
+from sast_git_leaks import process
 
 
 def main():
     logging.setup_logging(variables.LOG_FILENAME.absolute())
-    logger = logging.getLogger(variables.LOG_ENV)
     tools_names = variables.TOOLS.keys()
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--repo', help='name of the repo to scan', required=True)
@@ -129,6 +33,7 @@ def main():
     if not process(
         Path(args.repo),
         args.output,
+        variables,
         volume,
         args.limit,
         args.tools
